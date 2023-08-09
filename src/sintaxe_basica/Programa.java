1ee521f57ef8c5c0e25266e1c4f561af3eebf23f
Ed Carlos <edcarlos.1@icloud.com>
2023-08-08 22:30:26 -0300

    Incluindo a classe Programa

---
+++ src/sintaxe_basica/Programa.java
@@ -0,0 +1,9 @@
+package sintaxe_basica;
+    // Classe Programa
+public class Programa {
+	// Metodo Main 
+ public static void main(String[] args ) {
+	// Excrevendo o que será executado e exibido na tela
+	 System.out.println("Ola! Ed");
+ }
+}
